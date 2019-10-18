dc22f919239044d14fa3a528435fb68b1f33a81d
saudrix <saudrix@gmail.com>
2019-10-18 18:53:44 +0200

    Adding Classes Board and Taquin to deal with the game and manage the cells - Taquin 0.2.4 Frankenstein

---
+++ TaquinCodeBehind/Board.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaquinCodeBehind
+{
+    class Board
+    {
+        public enum Neighbours { Up, Down, Left, Right }
+        #region Attributes
+        private int _size;
+        private Neighbours _neighbours = new Neighbours();
+        #endregion
+
+        #region Properties
+        public Cell[,] Structure;
+        #endregion
+
+        #region Construct
+        public Board(int size)
+        {
+            _size = size;
+            Structure = new Cell[_size, _size];
+        }
+        public Board(Cell[,] board)
+        {
+            Structure = board;
+            _size = Structure.Length;
+        }
+        #endregion
+
+        #region Methods
+        public void FindEmptyOne(out int i, out int j)
+        {
+            FindCellByValue(out i, out j, "-");
+        }
+
+        public void FindEmptyTwo(out int i, out int j)
+        {
+            i = 0; j = 0;
+            for (int y = _size - 1; y >= 0; y--)
+                for (int x = _size - 1; x >= 0; x--)
+                {
+                    if (Structure[y, x].Value == "-")
+                    {
+                        i = y; j = x;
+                        break;
+                    }
+                }
+        }
+
+        public void FindCellByValue(out int i, out int j, string value)
+        {
+            i = 0; j = 0;
+            for (int y = 0; y < _size; y++)
+                for (int x = 0; x < _size; x++)
+                {
+                    if (Structure[y, x].Value == value)
+                    {
+                        i = y; j = x;
+                        break;
+                    }
+                }
+        }
+
+        public void ClearBoardStatus()
+        {
+            foreach (Cell cell in Structure) cell.ClearMoves();
+        }
+
+        public void CalculatePossibleMoves()
+        {
+            foreach(Cell cell in Structure)
+            {
+                if(cell.Value != "-")
+                { 
+                    List<Neighbours> neighbours = EvaluateNeighbours(cell);
+                    foreach (Neighbours n in neighbours)
+                        switch (n)
+                        {
+                            case Neighbours.Up: cell.SetMoves(Cell.Moves.Up);  break;
+                            case Neighbours.Down: cell.SetMoves(Cell.Moves.Down); break;
+                            case Neighbours.Left: cell.SetMoves(Cell.Moves.Left); break;
+                            case Neighbours.Right: cell.SetMoves(Cell.Moves.Right); break;
+                        }
+                }
+            }
+        }
+
+        public List<Neighbours> EvaluateNeighbours(Cell cell)
+        {
+            List<Neighbours> neighbours = new List<Neighbours>();
+            int i, j;
+            FindCellByValue(out i, out j, cell.Value);
+            if (i > 0) if(Structure[i - 1, j].Value == "-") neighbours.Add(Neighbours.Up);
+            if (i < _size - 1) if (Structure[i + 1, j].Value == "-") neighbours.Add(Neighbours.Down);
+            if (j > 0) if (Structure[i, j - 1].Value == "-") neighbours.Add(Neighbours.Left);
+            if (j < _size - 1) if (Structure[i, j + 1].Value == "-") neighbours.Add(Neighbours.Right);
+            return neighbours;
+        }
+        #endregion
+    }
+}
---
+++ TaquinCodeBehind/Taquin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaquinCodeBehind
+{
+    class Taquin
+    {
+        #region Attributes
+        private Board _board;
+        #endregion
+
+        #region Construct
+        public Taquin(int size)
+        {
+            Populate(size);
+            Shuffle();
+        }
+        #endregion
+
+        #region Methods
+        public void Populate(int size)
+        {
+            Cell[] cells = new Cell[size*size];
+            for(int i = 0; i < (size*size)-2; i++)
+            {
+                Cell cell = new Cell(i);
+                cells[i] = (cell);
+            }
+            cells[cells.Length - 2] = new Cell();
+            cells[cells.Length - 1] = new Cell();
+        }
+
+        public void Shuffle()
+        {
+            if(_board != null)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
