d7542e3558def2c30d43df39a0d0587877f203d7
saudrix <saudrix@gmail.com>
2019-12-03 09:27:47 +0100

    Adding parametrizer project and some minor changes to parameters

--- Parametrizer/ParametrizeSolver.cs
+++ Parametrizer/ParametrizeSolver.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaquinCodeBehind;
+using System.Diagnostics;
 
-namespace TaquinCodeBehind
+namespace Parametrizer
 {
-    public class Segments : AstarUni
+    class ParametrizeSolver
     {
         int Size { get; set; }
+        public List<int> Params { get; set; }
+        List<EvaluableBoard> _openSet = new List<EvaluableBoard>();
+        List<EvaluableBoard> _closedSet = new List<EvaluableBoard>();
+        EvaluableBoard _destination;
+        EvaluableBoard _currentBoard;
+        public TimeSpan performance { get; set; }
 
-        public Segments(IHeuristic heuristic): base(heuristic){}
+        public ParametrizeSolver( int param1, int param2, int param3)//, int param4, int param5)
+        {
+            param1 = param1 < 0 ? 0 : param1;
+            param2 = param2 < 0 ? 0 : param2;
+            param3 = param3 < 0 ? 0 : param3;
+            //param4 = param4 < 0 ? 0 : param4;
+            //param5 = param5 < 0 ? 0 : param5;
+            Params = new List<int>();
+            Params.Add(param1);
+            Params.Add(param2);
+            Params.Add(param3);
+            //Params.Add(param4);
+            //Params.Add(param5);
+        }
 
-        EvaluableBoard UnderStep(int rank)
+        Stopwatch UnderStep(int rank, Stopwatch chrono)
         {
             _destination = new EvaluableBoard(CreateStep(Size, rank));
             while (_openSet.Count > 0)
@@ -20,13 +41,14 @@ namespace TaquinCodeBehind
                 _currentBoard = _openSet[0];
                 if (_currentBoard.Equals(_destination))
                 {
-                    Console.WriteLine("=============== Step {0} ==============", rank);
-                    Console.WriteLine(_destination.Board);
-                    Console.WriteLine("======================================", rank);
-                    Console.WriteLine(_currentBoard.Board);
+                    //Console.WriteLine("=============== Step {0} ==============", rank);
+                    //Console.WriteLine(_destination.Board);
+                    //Console.WriteLine("======================================", rank);
+                    //Console.WriteLine(_currentBoard.Board);
                     if (rank == Size * Size - 3)
                     {
-                        return _currentBoard;
+                        chrono.Stop();
+                        return chrono;
                     }
                     else
                     {
@@ -35,7 +57,7 @@ namespace TaquinCodeBehind
                         _currentBoard.Cost = 0;
                         _currentBoard.Score = 0;
                         _openSet.Add(_currentBoard);
-                        return _currentBoard;
+                        break;
                     }
                 }
                 List<EvaluableBoard> holder = CreateChild(_currentBoard, rank);
@@ -54,83 +76,85 @@ namespace TaquinCodeBehind
                 _closedSet.Add(_currentBoard);
                 _openSet.Remove(_currentBoard);
                 _openSet = _openSet.OrderBy(b => b.Score).ToList();
+                if (chrono.Elapsed.Minutes >= 1)
+                    break;
             }
-            return null;
+            return chrono;
         }
 
-        public override List<Board> Solve(EvaluableBoard board)
+        public Stopwatch Solve(EvaluableBoard board)
         {
             // Innitialisation Step
             _openSet = new List<EvaluableBoard>();
             _closedSet = new List<EvaluableBoard>();
             Size = board.Board.Structure.GetLength(0);
             _openSet.Add(board);
-            EvaluableBoard result;
             // Solve loop
+            Stopwatch chrono = new Stopwatch();
+            chrono.Start();
             for (int rank = 0; rank < (Size * Size) - Size * 2; rank++)
             {
-                _currentBoard = UnderStep(rank);
+                chrono = UnderStep(rank, chrono);
+                if (chrono.Elapsed.Minutes >= 1)
+                    return chrono;
+
             }
             bool lineOne = true;
             int start = Size * Size - Size * 2;
             for (int rank = start; rank < start + Size; rank++)
             {
                 // Première ou deuxième place dans la colonne ?
-                _currentBoard = UnderStep(rank);
+                chrono = UnderStep(rank, chrono);
+                if (chrono.Elapsed.Minutes >= 1)
+                    return chrono;
+
                 int step = rank + Size;
                 if (step < Size * Size - 2)
                 {
-                    _currentBoard = UnderStep(step);
+                    chrono = UnderStep(step, chrono);
+                    if (chrono.Elapsed.Minutes >= 1)
+                        return chrono;
                 }
             }
-            return Unpile(_currentBoard);
+            chrono.Stop();
+            return chrono;
         }
 
         private int Eval(EvaluableBoard board, int step)
         {
             int score = 0;
             int optI, optJ, currI, currJ;
-            if(step < (Size * Size) - Size * 2)
-                for(int i = 0; i < step; i++)
-                {
-                    // Recherche de la position optimale
-                    Functions.pos2coord(out optI, out optJ, i, Size);
-                    // Recherche de la position courante
-                    board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(i));
-                    // Prise ne compte de la distance case destination
-                    score += 20 * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
-                }
-            else {
-                for (int i = 0; i < (Size * Size) - Size * 2; i++)
-                {
-                    // Recherche de la position optimale
-                    Functions.pos2coord(out optI, out optJ, i, Size);
-                    // Recherche de la position courante
-                    board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(i));
-                    // Prise ne compte de la distance case destination
-                    score += 20 * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
-                }
+            for (int i = 0; i < step; i++)
+            {
+                // Recherche de la position optimale
+                Functions.pos2coord(out optI, out optJ, i, Size);
+                // Recherche de la position courante
+                board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(i));
+                // Prise ne compte de la distance case destination
+                score += Math.Abs(i - step) * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
             }
             Functions.pos2coord(out optI, out optJ, step, Size);
             board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(step));
-            score += 8 * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
+            score += Params[0] * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
 
             int hole1I, hole1J, hole2I, hole2J;
             board.Board.FindEmptyOne(out hole1I, out hole1J);
             board.Board.FindEmptyTwo(out hole2I, out hole2J);
-            
             // Prise en compte de la distance trou case à placer
-            score += 3 * ( Math.Abs(hole1I - currI) + Math.Abs(hole1J - currJ));
-            score += 3 * ( Math.Abs(hole2I - currI) + Math.Abs(hole2J - currJ));
-            
-            
+            int distH1T = Math.Abs(hole1I - currI) + Math.Abs(hole1J - currJ);
+            int distH2T = Math.Abs(hole2I - currI) + Math.Abs(hole2J - currJ);
+
+            score += Params[1] * distH1T;//* Math.Min(distH1T, distH2T);
+            score += Params[1] * distH2T;//* Math.Max(distH1T, distH2T);
+
             // Prise en compte de la distance trou destination
             int distH1 = Math.Abs(optI - hole1I) + Math.Abs(optJ - hole1J);
             int distH2 = Math.Abs(optI - hole2I) + Math.Abs(optJ - hole2J);
 
             //score += 5 * distH1;
             //score += 5 * distH2;
-            score += 5 * Math.Min(distH1, distH2);
+            score += Params[2] * Math.Min(distH1, distH2);
+            //score += Params[4] * Math.Max(distH1, distH2);
             return score;
         }
 
@@ -138,7 +162,7 @@ namespace TaquinCodeBehind
         {
             Board board;
             List<Cell> structure = new List<Cell>();
-            if (rank < size * size - size * 2)
+            if(rank < size * size - size * 2)
             {
                 for (int i = 0; i <= rank; i++)
                 {
@@ -177,10 +201,10 @@ namespace TaquinCodeBehind
                 }
                 board = new Board(structure.ToArray());
                 int index = 0;
-                if (Size == 3)
+                if(Size == 3)
                 {
                     values = new List<int>() { 3, 6, 4, 5 };
-                    while (values[index] != rank)
+                    while(values[index] != rank)
                     {
                         int posI, posJ;
                         Functions.pos2coord(out posI, out posJ, values[index], Size);
@@ -206,17 +230,85 @@ namespace TaquinCodeBehind
             return board;
         }
 
+        // Might be transform as static
+        public static List<EvaluableBoard> CreateChild(EvaluableBoard board)
+        {
+            List<EvaluableBoard> neighbours = new List<EvaluableBoard>();
+
+            foreach (Cell cell in board)
+            {
+                int i, j;
+                board.Board.FindCellByValue(out i, out j, cell.Value);
+                if (cell.IsMovable())
+                {
+                    foreach (Cell.Moves move in cell.AvailableMoves)
+                    {
+                        EvaluableBoard neighbour = CopyBoard(board);
+                        neighbour.Cost = board.Cost;
+                        neighbour.Board.Move(neighbour.Board.Structure[i, j], move);
+                        neighbour.Previous = board;
+                        neighbours.Add(neighbour);
+                    }
+                }
+            }
+            return neighbours;
+        }
+
+        public static EvaluableBoard CopyBoard(EvaluableBoard board)
+        {
+            EvaluableBoard result = new EvaluableBoard(board.Score);
+            Cell[,] structure = new Cell[board.Size, board.Size];
+            foreach (Cell cell in board)
+            {
+                Cell newCell = new Cell(cell.Value);
+                int posI, posJ;
+                board.Board.FindCellByValue(out posI, out posJ, cell.Value);
+                structure[posI, posJ] = newCell;
+            }
+            // Might me proper
+            Cell emptyOne = new Cell("-");
+            Cell emptyTwo = new Cell("-");
+            int e1i, e1j, e2i, e2j;
+            board.Board.FindEmptyOne(out e1i, out e1j);
+            board.Board.FindEmptyTwo(out e2i, out e2j);
+            structure[e1i, e1j] = emptyOne;
+            structure[e2i, e2j] = emptyTwo;
+            // Till there is not really needed
+            result.Board = new Board(structure);
+            result.Size = result.Board.Structure.GetLength(0);
+            return result;
+        }
+
+        public bool FindPast(EvaluableBoard board)
+        {
+            bool result = false;
+            foreach (EvaluableBoard oldBoard in _closedSet)
+                if (oldBoard.Equals(board)) result = true;
+            return result;
+        }
+
+        public bool FindBest(EvaluableBoard board)
+        {
+            bool result = false;
+            foreach (EvaluableBoard currentBoard in _openSet)
+            {
+                if (board.Equals(currentBoard) && currentBoard.Cost <= board.Cost) result = true;
+                else if (board.Equals(currentBoard) && currentBoard.Cost > board.Cost) { currentBoard.Cost = board.Cost; }
+            }
+            return result;
+        }
+
         public static new List<EvaluableBoard> CreateChild(EvaluableBoard board, int step)
         {
             List<EvaluableBoard> neighbours = new List<EvaluableBoard>();
             int size = board.Board.Structure.GetLength(0);
             List<int> values;
-            if (size == 3) values = new List<int>() {0, 1, 2, 3, 4, 7, 5, 6 };
-            else values = new List<int>() {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 16, 21, 17, 22, 18, 19 };
+            if (size == 3) values = new List<int>() { 0, 1, 2, 3, 4, 7, 5, 6 };
+            else values = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 16, 21, 17, 22, 18, 19 };
             int maxIndex = values.IndexOf(step);
             foreach (Cell cell in board)
             {
-                if(cell.Value != "-")
+                if (cell.Value != "-")
                 {
                     int index = values.IndexOf(Convert.ToInt32(cell.Value));
                     if (index >= maxIndex)
---
+++ Parametrizer/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaquinCodeBehind;
+using System.Diagnostics;
+
+namespace Parametrizer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Setup();
+            int nbGen = 10;
+            int nbIndiv = 100;
+            int nbCible = 20;//20;
+
+            Random r = new Random();
+
+            List<ParametrizeSolver> _population = new List<ParametrizeSolver>();
+            List<EvaluableBoard> _targets = new List<EvaluableBoard>();
+
+            //Initialisation Création de la population
+            Console.Write("Initializing population : ");
+            for (int _ = 0; _ < nbIndiv; _++)
+            {
+                int max = 10;
+                //ParametrizeSolver current = new ParametrizeSolver(r.Next(0, max), r.Next(0, max), r.Next(0, max), r.Next(0, max), r.Next(0, max));
+                ParametrizeSolver current = new ParametrizeSolver(7, 4, 8);//,9,4);
+                Mutate(current, r);
+                _population.Add(current);
+            }
+            Console.Write(_population.Count + " solver created.");
+            // Initialisation : Creation des cibles
+            Console.Write(" - Initializing targets : ");
+            for(int _ = 0; _ < nbCible; _++)
+            {
+                Board b = new Board(5);
+                Fill(b);
+                //b = Shuffle(b, r);
+                EvaluableBoard target = new EvaluableBoard(b);
+                _targets.Add(target);
+            }
+            Console.Write(_targets.Count + " target created. \n \n");
+
+            // Life Loop
+            Console.WriteLine("Starting Life Loop :\n");
+            for (int i = 0; i < nbGen; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write("Starting generation {0}", i);
+                // Mélanger les cibles
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                // Updating targets
+                _targets = new List<EvaluableBoard>();
+                for (int _ = 0; _ < nbCible; _++)
+                {
+                    Board b = new Board(5);
+                    Fill(b);
+                    b = Shuffle(b, r, 100 * (i+1)); // Difficulté du mélange
+                    EvaluableBoard target = new EvaluableBoard(b);
+                    _targets.Add(target);
+                }
+                Console.WriteLine(" - All targets shuffled - Starting :");
+                // Calculating performances
+                Console.ForegroundColor = ConsoleColor.Black;
+                int top = Console.CursorTop;
+                foreach (ParametrizeSolver solver in _population)
+                {
+                    List<TimeSpan> _performances = new List<TimeSpan>();
+
+                    int progress = (_population.IndexOf(solver)+1);
+                    string progressStr = new String('=', progress);
+
+                    foreach (EvaluableBoard board in _targets)
+                    {
+                        //Console.SetCursorPosition(0, top + 1);
+                        //int inProgress = ((_targets.IndexOf(board)));
+                        //string inProgressStr = new String('=', inProgress);
+                        //Console.Write("\rSolver {0} at [" + inProgressStr + new String(' ', (nbCible - inProgress)) + "]\n", (_targets.IndexOf(board)));
+
+                        Stopwatch chrono = solver.Solve(board);
+                        TimeSpan perf = chrono.Elapsed;
+                        _performances.Add(perf);
+                    }
+                    
+                    Console.SetCursorPosition(0, top);
+                    Console.Write("\rProgress [" + progressStr + new String(' ', (nbIndiv - progress)) + "] - ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(progress + "% \n");
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    var averageTimespan = new TimeSpan(Convert.ToInt64(_performances.Average(ts => ts.Ticks)));
+                    solver.performance = averageTimespan;
+                }
+                _population = _population.OrderBy(s => s.performance).ToList();
+                ParametrizeSolver _currentBest = _population[0];
+                Console.ForegroundColor = ConsoleColor.Green;
+                //Console.Write("Result of generation {0} : Best solver mean time is {1}, with p1 = {2} - p2 = {3} - p3 = {4} - p4 = {5} - p5 = {6}\n",i, _currentBest.performance, _currentBest.Params[0], _currentBest.Params[1], _currentBest.Params[2], _currentBest.Params[3], _currentBest.Params[4]);
+                Console.Write("Result of generation {0} : Best solver mean time is {1}, with p1 = {2} - p2 = {3} - p3 = {4}\n", i, _currentBest.performance, _currentBest.Params[0], _currentBest.Params[1], _currentBest.Params[2]);
+
+                ParametrizeSolver _currentWorst = _population[nbIndiv - 1];
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                //Console.Write("Result of generation {0} : Worst solver mean time is {1}, with p1 = {2} - p2 = {3} - p3 = {4} - p4 = {5} - p5 = {6}\n\n", i, _currentWorst.performance, _currentWorst.Params[0], _currentWorst.Params[1], _currentWorst.Params[2], _currentBest.Params[3], _currentBest.Params[4]);
+                Console.Write("Result of generation {0} : Worst solver mean time is {1}, with p1 = {2} - p2 = {3} - p3 = {4}\n\n", i, _currentWorst.performance, _currentWorst.Params[0], _currentWorst.Params[1], _currentWorst.Params[2]);
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                // Dealing with generation decendance...
+
+                _population = _population.GetRange(0, nbIndiv / 2);
+                // Faire muter les anciens vecteur peut aider ?
+
+                while(_population.Count < nbIndiv - nbIndiv/4)
+                {
+                    ParametrizeSolver father = _population[r.Next(0, _population.Count)]; // /2 Pour favoriser les meilleurs pas forcément
+                    ParametrizeSolver mother = _population[r.Next(0, _population.Count)];// La meilleure solution
+                    int param1, param2, param3;//, param4, param5;
+                    int gene1, gene2, gene3;//, gene4, gene5;
+                    gene1 = r.Next(0, 2);
+                    gene2 = r.Next(0, 2);
+                    gene3 = r.Next(0, 2);
+                    //gene4 = r.Next(0, 2);
+                    //gene5 = r.Next(0, 2);
+                    param1 = gene1 == 0 ? father.Params[0] : mother.Params[0];
+                    param2 = gene2 == 0 ? father.Params[1] : mother.Params[1];
+                    param3 = gene3 == 0 ? father.Params[2] : mother.Params[2];
+                    //param4 = gene4 == 0 ? father.Params[3] : mother.Params[3];
+                    //param5 = gene5 == 0 ? father.Params[4] : mother.Params[4];
+                    ParametrizeSolver child = new ParametrizeSolver(param1, param2, param3);//, param4, param5);
+                    Mutate(child, r);
+                    _population.Add(child);
+                }
+                while(_population.Count < nbIndiv)
+                {
+                    int max = 10;
+                    ParametrizeSolver current = new ParametrizeSolver(r.Next(0, max), r.Next(0, max), r.Next(0, max));//, r.Next(0, max), r.Next(0, max));
+                    Mutate(current, r);
+                    _population.Add(current);
+                }
+            }
+
+            Console.Read();
+        }
+
+        static Board Shuffle(Board b, Random r, int diff)
+        {
+            int size = b.Structure.GetLength(0);
+            Taquin t = new Taquin(size);
+
+            for (int i = 0; i <= diff; i++)
+            {
+                foreach (Cell cell in t)
+                {
+                    if (cell.IsMovable())
+                    {
+                        if (r.Next(2) == 1) t.Move(cell);
+                    };
+                }
+                t.Board.ClearBoardStatus();
+                t.Board.CalculatePossibleMoves();
+            }
+            return t.Board;
+        }
+
+        static void Fill(Board b)
+        {
+            int size = b.Structure.GetLength(0);
+            for(int val = 0; val < size*size-2; val++)
+            {
+                Cell c = new Cell(val);
+                int posI, posJ;
+                Functions.pos2coord(out posI, out posJ, val, size);
+                b.Structure[posI, posJ] = c; 
+            }
+            for(int _ = 0; _ < 2; _++)
+            {
+                Cell c = new Cell("-");
+                b.Structure[4, 3 + _] = c;
+            }
+        }
+        static void Mutate(ParametrizeSolver solver, Random  r)
+        {
+            int luck;
+
+            int probMut = 2;
+            int mutRate = 1;
+
+            for (int i = 0; i < 3; i++)
+            {
+                luck = r.Next(10);
+                if(luck > probMut)
+                {
+                    if(luck > 5)
+                        solver.Params[i] += mutRate;
+                    else if(solver.Params[i]> 0) solver.Params[i] -= mutRate;
+                }
+            }
+        }
+
+        static void Setup()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WindowHeight = 69 - 20;
+            Console.WindowWidth = 266 - 40;
+            Console.Clear();
+        }
+    }
+}
