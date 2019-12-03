e07a230875bfc217b04d53f39bd22a90c3733e99
saudrix <saudrix@gmail.com>
2019-12-03 18:40:46 +0100

    Comment added to IHeuristic

--- TaquinCodeBehind/Heuristiques/IHeuristic.cs
+++ TaquinCodeBehind/Heuristiques/IHeuristic.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace TaquinCodeBehind
 {
+    /// <summary>
+    /// Interface à implémenter pour toute les heuristique
+    /// </summary>
     public interface IHeuristic
     {
+        // fonction a implémenter pour calculer le cout séparant deux tableaux
         int EvaluateBoard(Board currBoard, Board destBoard); 
     }
 }
