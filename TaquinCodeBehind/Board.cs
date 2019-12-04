c0a9d548dfc8782d532136e3028d8c7f9c6ad4c4
saudrix <saudrix@gmail.com>
2019-12-04 18:33:11 +0100

    Commented Board class

--- TaquinCodeBehind/Board.cs
+++ TaquinCodeBehind/Board.cs
@@ -9,28 +9,34 @@ namespace TaquinCodeBehind
 {
     public class Board : IEnumerable<Cell>
     {
+        // Enumeration des quatres mouvements possibles dans le jeu du Taquin
         public enum Neighbours { Up, Down, Left, Right }
+
         #region Attributes
         private int _size;
         #endregion
 
         #region Properties
+        // La structure contient toutes les cellules du tableau
         public Cell[,] Structure;
         #endregion
 
         #region Construct
+        // Constructeur d'un tableau de taille (size x size)
         public Board(int size)
         {
             _size = size;
             Structure = new Cell[_size, _size];
         }
 
+        // Constructeur d'un tableau à partir d'un tableau de cellules size by size
         public Board(Cell[,] board)
         {
             Structure = board;
             _size = Structure.GetLength(0);
         }
 
+        // Constructeur d'un tableau à partir d'un tableau de cellules size*size
         public Board(Cell[] cells)
         {
             if (cells.Length % 3 == 0) _size = 3;
@@ -47,14 +53,17 @@ namespace TaquinCodeBehind
         #endregion
 
         #region Methods
+        // Methode pour trouver le premier trou dans la grille
         public void FindEmptyOne(out int i, out int j)
         {
             FindCellByValue(out i, out j, "-");
         }
 
+        // Methode pour trouver le second trou dans la grille 
         public void FindEmptyTwo(out int i, out int j)
         {
             i = 0; j = 0;
+            // L'astuce est de parcourir le tableau en sens inverse !
             for (int y = _size - 1; y >= 0; y--)
                 for (int x = _size - 1; x >= 0; x--)
                 {
@@ -66,6 +75,7 @@ namespace TaquinCodeBehind
                 }
         }
 
+        // Methode permettant de trouver les coordonnées de n'importe qu'elle cellule par sa valeur
         public void FindCellByValue(out int i, out int j, string value)
         {
             i = 0; j = 0;
@@ -80,33 +90,34 @@ namespace TaquinCodeBehind
                 }
         }
 
+        // Permet de nettoyer les mouvements possible dans le tableau
         public void ClearBoardStatus()
         {
             foreach (Cell cell in Structure) cell.ClearMoves();
         }
 
+        // Permet de calculer tout les mouvement possibles dans le tableau
         public void CalculatePossibleMoves()
         {
             foreach(Cell cell in Structure)
             {
-                if(cell.Value != "-")
+                if(cell.Value != "-") // Un trou ne peu pas bouger
                 { 
-                    List<Neighbours> neighbours = EvaluateNeighbours(cell);
+                    List<Neighbours> neighbours = EvaluateNeighbours(cell); // Si la cellule à des mouvements possible
                     foreach (Neighbours n in neighbours)
                         switch (n)
                         {
+                            // On ajoute ces movement à la cellule
                             case Neighbours.Up: cell.SetMoves(Cell.Moves.Up);  break;
                             case Neighbours.Down: cell.SetMoves(Cell.Moves.Down); break;
                             case Neighbours.Left: cell.SetMoves(Cell.Moves.Left); break;
                             case Neighbours.Right: cell.SetMoves(Cell.Moves.Right); break;
                         }
-                    //string moves = "";
-                    //foreach (Cell.Moves move in cell.AvailableMoves) moves += move + " - ";
-                    //Console.WriteLine(cell.Value + " moves: " + moves);
                 }
             }
         }
 
+        // Permet de savoir si la cellule peut bouger dans un trou de son voisinage
         public List<Neighbours> EvaluateNeighbours(Cell cell)
         {
             List<Neighbours> neighbours = new List<Neighbours>();
@@ -119,6 +130,7 @@ namespace TaquinCodeBehind
             return neighbours;
         }
 
+        // Permet de bouger un cellule donnée dans un sens donnée
         public void Move(Cell cell, Cell.Moves move )
         {
             Cell holder;
@@ -126,6 +138,7 @@ namespace TaquinCodeBehind
             FindCellByValue(out i, out j, cell.Value);
             switch (move)
             {
+                // On écahnge les réference de cellule dans le tableau
                 case Cell.Moves.Up:
                     holder = Structure[i - 1, j];
                     Structure[i - 1, j] = cell;
@@ -147,10 +160,12 @@ namespace TaquinCodeBehind
                     Structure[i, j] = holder;
                     break;
             }
+            // On supprime les anciens mouvements et on calcule les nouveaux mouvements possible        
             ClearBoardStatus();
             CalculatePossibleMoves();
         }
 
+        // Permet d'afficher le board dans la console pour débuger plus simplement
         public override string ToString()
         {
             string result = "";
@@ -167,6 +182,10 @@ namespace TaquinCodeBehind
         #endregion
 
         #region IEnumerable
+        /// <summary>
+        /// Implementation de l'interface IEnumerable permettant de rendre Itérable le tableau
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<Cell> GetEnumerator()
         {
             int line = -1;
