a9a9aa2ad7a5f7de7fbfcdac71d91b6f19510ec3
saudrix <saudrix@gmail.com>
2019-12-04 19:04:22 +0100

    Adding comment to Cell class

--- TaquinCodeBehind/Cell.cs
+++ TaquinCodeBehind/Cell.cs
@@ -8,14 +8,14 @@ namespace TaquinCodeBehind
 {
     public class Cell
     {
+        // ensemble de toutes les valeurs possible pour un objet cellule
         string[] POSSIBLES_VALUES = new string[]{"-1","0","1","2","3","4","5","6","7","8","9","10","11","12","13","14",
                                                "15","16","17","18","19","20","21","22","23","-"};
+        // Liste des mouvement possible pour une cellule
         public enum Moves { Up, Down, Left, Right }
 
-
         #region Attributes
-        private List<Moves> _availableMoves = new List<Moves>();
-        private string _value;
+        private string _value; // Valeur de l'objet
         #endregion
 
         #region Properties
@@ -24,39 +24,51 @@ namespace TaquinCodeBehind
             get { return _value; }
             set
             {
+                // Avant d'assigner la valeur on regarde si c'est une valeur valide
                 if (POSSIBLES_VALUES.Contains(value)) _value = value;
             }
         }
-        public List<Moves> AvailableMoves { get { return _availableMoves; } }
+        // Liste des mouvements possibles pour une cellule dans sa position
+        public List<Moves> AvailableMoves { get; private set; } = new List<Moves>();
         #endregion
 
         #region Construct
+        // La cellule par défaut est un trou
         public Cell() { Value = "-"; }
 
+        // On peut lui passer une chaine de catactère
         public Cell(string value) { Value = value; }
 
+        // Ou un entier
         public Cell(int value) { Value = value.ToString(); }
         #endregion
 
         #region Methods
+        // Ajoute un mouvement à la liste des mouvements possible
         public void SetMoves(Moves move)
         {
-            _availableMoves.Add(move);
+            AvailableMoves.Add(move);
         }
+
+        // Ajoute une liste de mouvements aux mouvement possible
         public void SetMoves(Moves[] move)
         {
-            foreach (Moves m in move) _availableMoves.Add(m);
+            foreach (Moves m in move) AvailableMoves.Add(m);
         }
+
+        // Renvoie true si la cellule peut bouger
         public bool IsMovable()
         {
             if (AvailableMoves.Count > 0) return true;
             else return false;
         }
+
+        // Vide les mouvements possible de la cellule
         public void ClearMoves()
         {
-            _availableMoves = new List<Moves>();
+            AvailableMoves = new List<Moves>();
         }
-
+        
         public override string ToString()
         {
             return Value;
