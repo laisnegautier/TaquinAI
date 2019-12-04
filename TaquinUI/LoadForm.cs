6ba7b8e28d3e91622a8ae625229ffb4e358660b4
saudrix <saudrix@gmail.com>
2019-12-04 22:42:33 +0100

    Final commented loadForm

--- TaquinUI/LoadForm.cs
+++ TaquinUI/LoadForm.cs
@@ -11,104 +11,74 @@ using System.Windows.Forms;
 
 namespace TaquinUI
 {
+    /// <summary>
+    /// Fenêtre de gestion du chargement des exemple et des données enregistrées
+    /// </summary>
     public partial class LoadForm : Form
     {
+        #region Attributes
         private string _fileName;
         private string _path = @"./../../../Ressources";
-        private string _selectedFile;
-        private string[] _files;
         private Button _currentSelection;
-
-        public string SelectedFile { get { return _selectedFile; } }
-
         private bool mouseDown;
         private Point lastLocation;
+        #endregion
 
+        #region Properties
+        public string SelectedFile { get; private set; }
+        #endregion
+
+        #region Construct
         public LoadForm()
         {
             InitializeComponent();
             _fileName = "";
             LoadFilesButton();
         }
+        #endregion
 
+        #region Methods
         private void LoadFilesButton()
         {
             string[] _files = Directory.GetFiles(_path);
             int i = 0;
             foreach (string name in _files)
             {
-                // Define File selection button
+                // Création des boutons des fichiers
                 Button button = new Button
                 {
+                    // Contenu
                     Text = name.Substring(_path.Length + 1),
                     Width = filePanel.Width,
                     Height = 50,
-                    // Style
+                    // Définition du style
                     BackColor = Color.FromArgb(231, 217, 218),
                     ForeColor = Color.FromArgb(69, 94, 41),
                     FlatStyle = FlatStyle.Flat
                 };
                 button.FlatAppearance.BorderSize = 0;
                 button.Top = i * button.Height + i;
-                // Events
+                // Ajouts de la gestion des evenements
                 button.Click += (s, e) => SelectFileButton_Click(s, e);
                 button.MouseEnter += (s, e) => Button_MouseEnter(s, e);
                 button.MouseLeave += (s, e) => Button_MouseLeave(s, e);
-                // Adding the control
+                // Ajout du control au panel des fichiers
                 filePanel.Controls.Add(button);
                 if (i == 0) { _currentSelection = button; _fileName = button.Text; }
                 i++;
             }
             ButtonSetFocus(_currentSelection);
         }
+        #endregion
 
-        private void Button_MouseEnter(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            ButtonSetFocus(button);
-        }
-
-        private void Button_MouseLeave(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            //if ((button == sizeButton3 && _selectedSize == 3) || (button == sizeButton5 && _selectedSize == 5)) ;
-            if (button == _currentSelection) ;
-            else ButtonUnsetFocus(button);
-        }
-
-        private void ButtonSetFocus(Button button)
-        {
-            button.BackColor = Color.FromArgb(255, 143, 143);
-            button.ForeColor = Color.FromArgb(252, 252, 250);
-        }
-
-        private void ButtonUnsetFocus(Button button)
-        {
-            button.BackColor = Color.FromArgb(231, 217, 218);
-            button.ForeColor = Color.FromArgb(66, 94, 41);
-        }
-
+        #region UIMethods_Misceallanous
+        // Fonction pour fermer la fenêtre
         private void CloseLoadButton_Click(object sender, EventArgs e)
         {
             Hide();
         }
 
-        private void SelectFileButton_Click(object sender, EventArgs e)
-        {
-            ButtonUnsetFocus(_currentSelection);
-            Button button = (Button)sender;
-            _currentSelection = button;
-            ButtonSetFocus(button);
-            _fileName = button.Text;
-            Console.WriteLine(_selectedFile);
-        }
-
-        private void LoadButton_Click(object sender, EventArgs e)
-        {
-            _selectedFile = _path +"/"+ _fileName;
-            Close();   
-        }
-
+        // Foncion permettants de drag le form
         private void dragBorder_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
@@ -131,9 +101,66 @@ namespace TaquinUI
             mouseDown = false;
         }
 
+        // Fonction pour minimiser la fenêtre
         private void minimizeButton_Click(object sender, EventArgs e)
         {
             WindowState = FormWindowState.Minimized;
         }
+        #endregion
+
+        #region UIMethods_Fonctionnalities
+        // Fonction trigger lorsqu'un bouton raise un MouseEnter event
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            ButtonSetFocus(button);
+        }
+
+        // Fonction trigger lorsqu'un bouton raise un MouseLeave event
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            //if ((button == sizeButton3 && _selectedSize == 3) || (button == sizeButton5 && _selectedSize == 5)) ;
+            if (button == _currentSelection) ;
+            else ButtonUnsetFocus(button);
+        }
+
+        // Fonction permettant de mettre en focus un boutton
+        private void ButtonSetFocus(Button button)
+        {
+            button.BackColor = Color.FromArgb(255, 143, 143);
+            button.ForeColor = Color.FromArgb(252, 252, 250);
+        }
+
+        // Fonction permettant d'enlever le focus d'un boutton
+        private void ButtonUnsetFocus(Button button)
+        {
+            button.BackColor = Color.FromArgb(231, 217, 218);
+            button.ForeColor = Color.FromArgb(66, 94, 41);
+        }
+        #endregion
+
+        #region UIMethods_Interactions
+        // Fonction déclenchée au click sur un bouton fichier
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            // Gestion de l'affichage
+            ButtonUnsetFocus(_currentSelection);
+            Button button = (Button)sender;
+            _currentSelection = button;
+            ButtonSetFocus(button);
+            // Selection du fichier associer
+            _fileName = button.Text;
+            Console.WriteLine(SelectedFile);
+        }
+
+        // Fonction permettant de charger le fichier choisis
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            SelectedFile = _path + "/" + _fileName;
+            // On ferme la fenêtre une fois le chemin créé
+            Close();
+        }
+        #endregion
     }
 }
