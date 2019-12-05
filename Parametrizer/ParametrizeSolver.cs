fdd10519aa8e3c26c2a75fc00cc096a447adb2aa
saudrix <saudrix@gmail.com>
2019-12-05 09:57:03 +0100

    Commented parametrize solver

--- Parametrizer/ParametrizeSolver.cs
+++ Parametrizer/ParametrizeSolver.cs
@@ -8,48 +8,75 @@ using System.Diagnostics;
 
 namespace Parametrizer
 {
+    /// <summary>
+    /// Classe reprenant la structure du solver méthode humaine 
+    /// et le rend facilement paramétrable afin de l'optimiser
+    /// </summary>
     class ParametrizeSolver
     {
-        int Size { get; set; }
-        public List<int> Params { get; set; }
+        #region Attributes
         List<EvaluableBoard> _openSet = new List<EvaluableBoard>();
         List<EvaluableBoard> _closedSet = new List<EvaluableBoard>();
         EvaluableBoard _destination;
         EvaluableBoard _currentBoard;
-        public TimeSpan performance { get; set; }
+        #endregion
 
-        public ParametrizeSolver( int param1, int param2, int param3)//, int param4, int param5)
+        #region Properties
+        int Size { get; set; }
+        public List<int> Params { get; set; }
+        public TimeSpan performance { get; set; }
+        #endregion
+
+        #region Construct
+        // Constructeur pour une heuristique à trois paramètres
+        public ParametrizeSolver( int param1, int param2, int param3)
         {
+            // vérification et assignation des valeurs
             param1 = param1 < 0 ? 0 : param1;
             param2 = param2 < 0 ? 0 : param2;
             param3 = param3 < 0 ? 0 : param3;
-            //param4 = param4 < 0 ? 0 : param4;
-            //param5 = param5 < 0 ? 0 : param5;
             Params = new List<int>();
             Params.Add(param1);
             Params.Add(param2);
             Params.Add(param3);
-            //Params.Add(param4);
-            //Params.Add(param5);
         }
 
+        // Constructeur pour une heuristique à cinq paramètres
+        public ParametrizeSolver(int param1, int param2, int param3, int param4, int param5)
+        {
+            // vérification et assignation des valeurs
+            param1 = param1 < 0 ? 0 : param1;
+            param2 = param2 < 0 ? 0 : param2;
+            param3 = param3 < 0 ? 0 : param3;
+            param4 = param4 < 0 ? 0 : param4;
+            param5 = param5 < 0 ? 0 : param5;
+            Params = new List<int>();
+            Params.Add(param1);
+            Params.Add(param2);
+            Params.Add(param3);
+            Params.Add(param4);
+            Params.Add(param5);
+        }
+        #endregion
+
+        #region Methods_Solving
+        // Fonction permettant de résoudre 
         Stopwatch UnderStep(int rank, Stopwatch chrono)
         {
             _destination = new EvaluableBoard(CreateStep(Size, rank));
+            // Boucle de résolution
             while (_openSet.Count > 0)
             {
                 _currentBoard = _openSet[0];
                 if (_currentBoard.Equals(_destination))
                 {
-                    //Console.WriteLine("=============== Step {0} ==============", rank);
-                    //Console.WriteLine(_destination.Board);
-                    //Console.WriteLine("======================================", rank);
-                    //Console.WriteLine(_currentBoard.Board);
+                    // Si l'on est à la dernière étape on stop le chrono
                     if (rank == Size * Size - 3)
                     {
                         chrono.Stop();
                         return chrono;
                     }
+                    // Sinon on nettoie le solver pour la prochaine étape
                     else
                     {
                         _openSet = new List<EvaluableBoard>();
@@ -60,6 +87,7 @@ namespace Parametrizer
                         break;
                     }
                 }
+                // Gestion de la descendance
                 List<EvaluableBoard> holder = CreateChild(_currentBoard, rank);
                 foreach (EvaluableBoard testBoard in holder)
                 {
@@ -67,47 +95,52 @@ namespace Parametrizer
                     else
                     {
                         testBoard.Cost += 1;
-                        // Evaluation d'une heuristique humaine
+                        // Evaluation d'une heuristique humaine spécifique
                         int thisHumanHeuri = Eval(testBoard, rank);
-                        testBoard.Score = testBoard.Cost + thisHumanHeuri;//Heuristic.EvaluateBoard(testBoard.Board, _destination.Board);
+                        testBoard.Score = testBoard.Cost + thisHumanHeuri;
                         _openSet.Add(testBoard);
                     }
                 }
                 _closedSet.Add(_currentBoard);
                 _openSet.Remove(_currentBoard);
                 _openSet = _openSet.OrderBy(b => b.Score).ToList();
+                // Si le solver dépasse la minute on l'interrompt
                 if (chrono.Elapsed.Minutes >= 1)
                     break;
             }
             return chrono;
         }
 
+        // Méthode de résolution globlae
         public Stopwatch Solve(EvaluableBoard board)
         {
-            // Innitialisation Step
+            // Etape d'initialisation
             _openSet = new List<EvaluableBoard>();
             _closedSet = new List<EvaluableBoard>();
             Size = board.Board.Structure.GetLength(0);
+            // On prend le premier état
             _openSet.Add(board);
-            // Solve loop
+            // On init et on démarre un Chronomètre
             Stopwatch chrono = new Stopwatch();
             chrono.Start();
+            // On parcours d'abord en ligne case par case
             for (int rank = 0; rank < (Size * Size) - Size * 2; rank++)
             {
+                // On résoud chaque étape une par une en placant la case cible
                 chrono = UnderStep(rank, chrono);
                 if (chrono.Elapsed.Minutes >= 1)
                     return chrono;
 
             }
-            bool lineOne = true;
             int start = Size * Size - Size * 2;
+            // On parcours ensuite les deux dernière lignes en colonnes
             for (int rank = start; rank < start + Size; rank++)
             {
-                // Première ou deuxième place dans la colonne ?
+                // On fait la ligne du haut 
                 chrono = UnderStep(rank, chrono);
                 if (chrono.Elapsed.Minutes >= 1)
                     return chrono;
-
+                // Puis la ligne du bas
                 int step = rank + Size;
                 if (step < Size * Size - 2)
                 {
@@ -116,23 +149,18 @@ namespace Parametrizer
                         return chrono;
                 }
             }
+            // On arrête le chrono si on a fini
             chrono.Stop();
             return chrono;
         }
 
+        // Implémentation de l'Heuristique spécifique à cette méthode humaine
         private int Eval(EvaluableBoard board, int step)
         {
             int score = 0;
             int optI, optJ, currI, currJ;
-            for (int i = 0; i < step; i++)
-            {
-                // Recherche de la position optimale
-                Functions.pos2coord(out optI, out optJ, i, Size);
-                // Recherche de la position courante
-                board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(i));
-                // Prise ne compte de la distance case destination
-                score += Math.Abs(i - step) * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
-            }
+
+            // Prise en compte de la case courante a placer
             Functions.pos2coord(out optI, out optJ, step, Size);
             board.Board.FindCellByValue(out currI, out currJ, Convert.ToString(step));
             score += Params[0] * (Math.Abs(optI - currI) + Math.Abs(optJ - currJ));
@@ -143,37 +171,38 @@ namespace Parametrizer
             // Prise en compte de la distance trou case à placer
             int distH1T = Math.Abs(hole1I - currI) + Math.Abs(hole1J - currJ);
             int distH2T = Math.Abs(hole2I - currI) + Math.Abs(hole2J - currJ);
-
-            score += Params[1] * distH1T;//* Math.Min(distH1T, distH2T);
-            score += Params[1] * distH2T;//* Math.Max(distH1T, distH2T);
+            score += Params[1] * distH1T;
+            score += Params[1] * distH2T;
 
             // Prise en compte de la distance trou destination
             int distH1 = Math.Abs(optI - hole1I) + Math.Abs(optJ - hole1J);
             int distH2 = Math.Abs(optI - hole2I) + Math.Abs(optJ - hole2J);
-
-            //score += 5 * distH1;
-            //score += 5 * distH2;
             score += Params[2] * Math.Min(distH1, distH2);
-            //score += Params[4] * Math.Max(distH1, distH2);
+
             return score;
         }
 
+        // Permet de créer une étape intermédiaire pour n'importe quel rang
         private Board CreateStep(int size, int rank)
         {
             Board board;
             List<Cell> structure = new List<Cell>();
+            // On rempli normalement le tableau si l'on est avant les deux dernières lignes
             if(rank < size * size - size * 2)
             {
+                // Ajout des valeurs
                 for (int i = 0; i <= rank; i++)
                 {
                     Cell cell = new Cell(i);
                     structure.Add(cell);
                 }
+                // On ajoute ensuite les cases non pertinentes (-1)
                 for (int i = rank; i < (size * size - 2) - 1; i++)
                 {
                     Cell empty = new Cell(-1);
                     structure.Add(empty);
                 }
+                // On rajoute les deux trous
                 for (int i = 0; i < 2; i++)
                 {
                     Cell hole = new Cell("-");
@@ -181,8 +210,10 @@ namespace Parametrizer
                 }
                 board = new Board(structure.ToArray());
             }
+            // On rempli les deux dernières lignes différemment
             else
             {
+                // On remplis d'abord le début normalement
                 List<int> values;
                 for (int i = 0; i <= Size * Size - Size * 2; i++)
                 {
@@ -201,9 +232,11 @@ namespace Parametrizer
                 }
                 board = new Board(structure.ToArray());
                 int index = 0;
+                // Ensuite on dissocie 3x3
                 if(Size == 3)
                 {
                     values = new List<int>() { 3, 6, 4, 5 };
+                    // ON ajoute les valeurs en colonnes
                     while(values[index] != rank)
                     {
                         int posI, posJ;
@@ -212,9 +245,11 @@ namespace Parametrizer
                         index++;
                     }
                 }
+                // et les Taquin 5x5
                 else
                 {
                     values = new List<int>() { 15, 20, 16, 21, 17, 22, 18, 19 };
+                    // On ajoute alors les valeurs en colonnes
                     while (values[index] != rank)
                     {
                         int posI, posJ;
@@ -229,31 +264,10 @@ namespace Parametrizer
             }
             return board;
         }
+        #endregion
 
-        // Might be transform as static
-        public static List<EvaluableBoard> CreateChild(EvaluableBoard board)
-        {
-            List<EvaluableBoard> neighbours = new List<EvaluableBoard>();
-
-            foreach (Cell cell in board)
-            {
-                int i, j;
-                board.Board.FindCellByValue(out i, out j, cell.Value);
-                if (cell.IsMovable())
-                {
-                    foreach (Cell.Moves move in cell.AvailableMoves)
-                    {
-                        EvaluableBoard neighbour = CopyBoard(board);
-                        neighbour.Cost = board.Cost;
-                        neighbour.Board.Move(neighbour.Board.Structure[i, j], move);
-                        neighbour.Previous = board;
-                        neighbours.Add(neighbour);
-                    }
-                }
-            }
-            return neighbours;
-        }
-
+        #region Method_FromSolver
+        // Méthode permettant de copier un tableau pour briser les références
         public static EvaluableBoard CopyBoard(EvaluableBoard board)
         {
             EvaluableBoard result = new EvaluableBoard(board.Score);
@@ -279,6 +293,7 @@ namespace Parametrizer
             return result;
         }
 
+        // Fonciton de recherche des antécédents
         public bool FindPast(EvaluableBoard board)
         {
             bool result = false;
@@ -298,19 +313,23 @@ namespace Parametrizer
             return result;
         }
 
-        public static new List<EvaluableBoard> CreateChild(EvaluableBoard board, int step)
+        // Création des enfants spécifique à la méthode humaine
+        public static List<EvaluableBoard> CreateChild(EvaluableBoard board, int step)
         {
             List<EvaluableBoard> neighbours = new List<EvaluableBoard>();
             int size = board.Board.Structure.GetLength(0);
             List<int> values;
+            // Valeurs possibles dans les tableaux
             if (size == 3) values = new List<int>() { 0, 1, 2, 3, 4, 7, 5, 6 };
             else values = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20, 16, 21, 17, 22, 18, 19 };
             int maxIndex = values.IndexOf(step);
+            // On parcours toutes les cellules du tableau
             foreach (Cell cell in board)
             {
                 if (cell.Value != "-")
                 {
                     int index = values.IndexOf(Convert.ToInt32(cell.Value));
+                    // On bloque le mouvement des cases déjà positionnées
                     if (index >= maxIndex)
                     {
                         int i, j;
@@ -331,5 +350,6 @@ namespace Parametrizer
             }
             return neighbours;
         }
+        #endregion
     }
 }
